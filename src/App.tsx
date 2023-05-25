321f20aa5386eb663625b4f068dc09d7a19a28c0
FaizanKhanPathan <pathanfaizan.1999@gmail.com>
2023-05-25 11:18:24 +0530

    assdc
    sdcdds

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <StudioBoard />
-      <Slidebar />
+      {/* <Slidebar /> */}
     </>
   );
 }
--- src/Components/StudioBoard.tsx
+++ src/Components/StudioBoard.tsx
@@ -3,42 +3,47 @@ import BackLog from "./BackLog";
 import InProgress from "./InProgress";
 import Review from "./Review";
 import Complete from "./Complete";
-import { BsArrowDownCircle, BsArrowUpCircle } from "react-icons/bs"
+import { BsArrowDownCircle, BsArrowUpCircle } from "react-icons/bs";
 import Header from "./Header";
-
+import Slidebar from "./Slidebar";
 
 const StudioBoard = () => {
-
-    const [icon, setIcon] = useState(false)
+  const [icon, setIcon] = useState(false);
 
   return (
-    <div className="flex flex-col justify-center items-center w-[100vw] h-[100vh]">
-        <Header/>
-      <div className="border border-black w-[70vw] h-[70vh] pl-8 pr-8 pt-6">
-        <div className="flex justify-between">
-          <div className="text-xl text-black font-semibold flex items-center gap-3">
-            <div>Studio Board</div>
-            <div onClick={()=>setIcon(!icon)}>
-                {
-                   !icon ?  
-                <BsArrowDownCircle/> 
-                : <BsArrowUpCircle/>
-                }
-                </div>
+      <div className="flex flex-col justify-center items-center w-[100vw] h-[100vh]">
+      <Header />
+      <div className="flex flex-row">
+        <Slidebar />
+        <div className="border border-black w-[70vw] h-[70vh] pl-8 pr-8 pt-6">
+          <div className="flex justify-between">
+            <div className="text-xl text-black font-semibold flex items-center gap-3">
+              <div>Studio Board</div>
+              <div onClick={() => setIcon(!icon)}>
+                {!icon ? <BsArrowDownCircle /> : <BsArrowUpCircle />}
+              </div>
+            </div>
+            <div>Sohil</div>
           </div>
-          <div>Sohil</div>
-        </div>
 
-        <div className="flex flex-row justify-between items-center pt-6">
-        {
-            !icon && <>
-            <div><BackLog/></div>
-            <div><InProgress/></div>
-            <div><Review/></div>
-            <div><Complete/></div>
-            </>
-        }
-
+          <div className="flex flex-row justify-between items-center pt-6">
+            {!icon && (
+              <>
+                <div>
+                  <BackLog />
+                </div>
+                <div>
+                  <InProgress />
+                </div>
+                <div>
+                  <Review />
+                </div>
+                <div>
+                  <Complete />
+                </div>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </div>
