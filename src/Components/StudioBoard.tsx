0fc312e0ca2bbd9d91ee3435da00dea01f061b32
FaizanKhanPathan <pathanfaizan.1999@gmail.com>
2023-05-25 11:00:32 +0530

    svdfsv
    dsfvdfv

--- src/Components/StudioBoard.tsx
+++ src/Components/StudioBoard.tsx
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import BackLog from "./BackLog";
 import InProgress from "./InProgress";
 import Review from "./Review";
 import Complete from "./Complete";
-import { BsArrowDownCircle } from "react-icons/bs"
+import { BsArrowDownCircle, BsArrowUpCircle } from "react-icons/bs"
 
 
 const StudioBoard = () => {
+
+    const [icon, setIcon] = useState(false)
+
   return (
     <div className="flex justify-center items-center w-[100vw] h-[100vh]">
       <div className="border border-black w-[70vw] h-[70vh] pl-8 pr-8 pt-6">
         <div className="flex justify-between">
           <div className="text-xl text-black font-semibold flex items-center gap-3">
             <div>Studio Board</div>
-            <div><BsArrowDownCircle/></div>
+            <div onClick={()=>setIcon(!icon)}>
+                {
+                   !icon ?  
+                <BsArrowDownCircle/> 
+                : <BsArrowUpCircle/>
+                }
+                </div>
           </div>
           <div>Sohil</div>
         </div>
 
         <div className="flex flex-row justify-between items-center pt-6">
+            
           <div><BackLog/></div>
           <div><InProgress/></div>
           <div><Review/></div>
