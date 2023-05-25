ce7c1fabc3f516c32ad2ed078412876aab30d23c
Sohail037 <alexjason261@gmail.com>
2023-05-25 16:38:38 +0530

    okokok

--- src/Components/Slidebar.tsx
+++ src/Components/Slidebar.tsx
@@ -17,28 +17,28 @@ const Slidebar = () => {
         setBoards(true)
     },[])
   return (
-    <div className='w-[8vw] h-[76.4vh] relative bottom-12 bg-white drop-shadow-lg'>
+    <div className='w-[8vw] h-[76.1vh] relative bottom-12 bg-white drop-shadow-lg'>
         <div className='h-[3rem] flex items-center justify-center font-semibold text-white w-[100%] bg-[#878FDC]'>
 
         <h1>hussle</h1>
         </div>
-        <div className={managers  ? 'text-[#2a4e85] bg-[#ebecebec] cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center':' cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center'} onClick={()=>{setManagers(true);setBoards(false);setSchedual(false);setReports(false);setSetting(false)}}>
+        <div className={managers  ? 'text-[#2a4e85] border-l-2 border-blue-400 cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center':'border-l-2 border-white cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center'} onClick={()=>{setManagers(true);setBoards(false);setSchedual(false);setReports(false);setSetting(false)}}>
             <RiGroupLine className='text-xl'/>
             <span className='font-semibold'>Managers</span>
         </div>
-        <div className={boards  ? 'text-[#2a4e85] bg-[#ebecebec]  cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center':' cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center'} onClick={()=>{setBoards(true);setManagers(false);setSchedual(false);setReports(false);setSetting(false)}}>
+        <div className={boards  ? 'text-[#2a4e85] border-l-2 border-blue-400 cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center':'border-l-2 border-white cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center'} onClick={()=>{setBoards(true);setManagers(false);setSchedual(false);setReports(false);setSetting(false)}}>
             <CgDatabase className='text-xl'/>
             <span className='font-semibold'>Boards</span>
         </div>
-        <div className={schedual  ? 'text-[#2a4e85] bg-[#ebecebec]  cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center':' cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center bg-white'} onClick={()=>{setSchedual(true);setBoards(false);setManagers(false);setReports(false);setSetting(false)}}>
+        <div className={schedual  ? 'text-[#2a4e85] border-l-2 border-blue-400  cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center':'border-l-2 border-white cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center bg-white'} onClick={()=>{setSchedual(true);setBoards(false);setManagers(false);setReports(false);setSetting(false)}}>
             <HiOutlineCalendar className='text-xl'/>
             <span className='font-semibold'>Schedual</span>
         </div>
-        <div className={Reports  ? 'text-[#2a4e85] bg-[#ebecebec]  cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center': ' cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center'} onClick={()=>{setReports(true);setSchedual(false);setBoards(false);setManagers(false);setSetting(false)}}>
+        <div className={Reports  ? 'text-[#2a4e85] border-l-2 border-blue-400 cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center': 'border-l-2 border-white cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center'} onClick={()=>{setReports(true);setSchedual(false);setBoards(false);setManagers(false);setSetting(false)}}>
             <MdInsertChart className='text-xl'/>
             <span className='font-semibold'>Reports</span>
         </div>
-        <div className={setting  ? 'mt-[9.5rem] text-[#2a4e85] bg-[#ebecebec]  cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center': 'mt-[9.5rem] cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center'} onClick={()=>{setSetting(true);setSchedual(false);setBoards(false);setManagers(false);setReports(false)}}>
+        <div className={setting  ? 'mt-[9.5rem] text-[#2a4e85] border-l-2 border-blue-400  cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center': 'border-l-2 border-white mt-[9.5rem] cursor-pointer justify-center w-[100%] h-[10vh] flex flex-col items-center'} onClick={()=>{setSetting(true);setSchedual(false);setBoards(false);setManagers(false);setReports(false)}}>
             <AiOutlineSetting className='text-xl'/>
             <span className='font-semibold'>Setting</span>
         </div>
