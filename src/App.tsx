75128fcbb539e5923ecfa3804c576707cc17bc84
FaizanKhanPathan <pathanfaizan.1999@gmail.com>
2023-05-25 10:56:17 +0530

    dfsvdfv
    dfvdfv

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import StudioBoard from "./Components/StudioBoard";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-          
-    </h1>
+      <StudioBoard />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/StudioBoard.tsx
+++ src/Components/StudioBoard.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import BackLog from "./BackLog";
+import InProgress from "./InProgress";
+import Review from "./Review";
+import Complete from "./Complete";
+import { BsArrowDownCircle } from "react-icons/bs"
+
 
 const StudioBoard = () => {
   return (
-    <div>StudioBoard</div>
-  )
-}
+    <div className="flex justify-center items-center w-[100vw] h-[100vh]">
+      <div className="border border-black w-[70vw] h-[70vh] pl-8 pr-8 pt-6">
+        <div className="flex justify-between">
+          <div className="text-xl text-black font-semibold flex items-center gap-3">
+            <div>Studio Board</div>
+            <div><BsArrowDownCircle/></div>
+          </div>
+          <div>Sohil</div>
+        </div>
 
-export default StudioBoard
+        <div className="flex flex-row justify-between items-center pt-6">
+          <div><BackLog/></div>
+          <div><InProgress/></div>
+          <div><Review/></div>
+          <div><Complete/></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StudioBoard;
